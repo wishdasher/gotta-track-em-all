26a7fbede85e87aefea353458fb4abff395603bc
Sophie Mori <ksmori14@gmail.com>
2018-05-04 00:01:24 -0400

    Fix store

--- scripts/search.js
+++ scripts/search.js
@@ -1,34 +1,9 @@
-var content = [
-  { title: 'Andorra' },
-  { title: 'United Arab Emirates' },
-  { title: 'Afghanistan' },
-  { title: 'Antigua' },
-  { title: 'Anguilla' },
-  { title: 'Albania' },
-  { title: 'Armenia' },
-  { title: 'Netherlands Antilles' },
-  { title: 'Angola' },
-  { title: 'Argentina' },
-  { title: 'American Samoa' },
-  { title: 'Austria' },
-  { title: 'Australia' },
-  { title: 'Aruba' },
-  { title: 'Aland Islands' },
-  { title: 'Azerbaijan' },
-  { title: 'Bosnia' },
-  { title: 'Barbados' },
-  { title: 'Bangladesh' },
-  { title: 'Belgium' },
-  { title: 'Burkina Faso' },
-  { title: 'Bulgaria' },
-  { title: 'Bahrain' },
-  { title: 'Burundi' }
-  // etc
-];
 
 var searchAll = true;
 
 var setup = () => {
 
+	console.log(cards);
 }
 document.addEventListener("DOMContentLoaded", setup);
+
--- scripts/store.js
+++ scripts/store.js
@@ -1,23 +1,74 @@
-
-/* CREATE ALL DATABASE CARDS */
-
-// TODO: load json cards
-
 var cards = {
-}
+	"cards": [
+	{
+		"id": 1, "name": "Absol", "src": "img/absol.png", "set": "Unleashed", "in_collection": true, "in_wishlist": false,
+	},
+	{
+		"id": 2, "name": "Arcanine", "src": "img/arcanine.png", "set": "Unleashed", "in_collection": true, "in_wishlist": false,
+	},
+	{
+		"id": 3, "name": "Bulbasaur", "src": "img/bulbasaur.png", "set": "Secret Wonders", "in_collection": true, "in_wishlist": false,
+	},
+	{
+		"id": 4, "name": "Celebi", "src": "img/celebi.png", "set": "Secret Wonders", "in_collection": false, "in_wishlist": false,
+	},
+	{
+		"id": 5, "name": "Charizard", "src": "img/harizard.png", "set": "Unleashed", "in_collection": true, "in_wishlist": false,
+	},
+	{
+		"id": 6, "name": "Charmander", "src": "img/charmander.png", "set": "Rising Rivals", "in_collection": false, "in_wishlist": false,
+	},
+	{
+		"id": 7, "name": "Espeon", "src": "img/espeon.png", "set": "Neo Destiny", "in_collection": true, "in_wishlist": false,
+	},
+	{
+		"id": 8, "name": "Haunter", "src": "img/haunter.png", "set": "Rising Rivals", "in_collection": true, "in_wishlist": false,
+	},
+	{
+		"id": 9, "name": "Jirachi", "src": "img/jirachi.png", "set": "Neo Destiny", "in_collection": true, "in_wishlist": false,
+	},
+	{
+		"id": 10, "name": "Latias", "src": "img/latias.png", "set": "Neo Destiny", "in_collection": true, "in_wishlist": false,
+	},
+	{
+		"id": 11, "name": "Mewtwo", "src": "img/mewtwo.png", "set": "Unleashed", "in_collection": false, "in_wishlist": false,
+	},
+	{
+		"id": 12, "name": "Pichu", "src": "img/pichu.png", "set": "Secret Wonders", "in_collection": false, "in_wishlist": false,
+	},
+	{
+		"id": 13, "name": "Pichu", "src": "img/pichu2.png", "set": "Neo Destiny", "in_collection": false, "in_wishlist": false,
+	},
+	{
+		"id": 14, "name": "Pikachu", "src": "img/pikachu.png", "set": "Neo Destiny", "in_collection": false, "in_wishlist": false,
+	},
+	{
+		"id": 15, "name": "Pikachu", "src": "img/pikachu2.png", "set": "Secret Wonders", "in_collection": true, "in_wishlist": false,
+	},
+	{
+		"id": 16, "name": "Pikachu", "src": "img/pikachu3.png", "set": "Unleashed", "in_collection": true, "in_wishlist": false,
+	},
+	{
+		"id": 17, "name": "Pikachu", "src": "img/pikachu4.png", "set": "Rising Rivals", "in_collection": false, "in_wishlist": false,
+	},
+	{
+		"id": 18, "name": "Raichu", "src": "img/raichu.png", "set": "Rising Rivals", "in_collection": false, "in_wishlist": false,
+	},
+	{
+		"id": 19, "name": "Sceptile", "src": "img/sceptile.png", "set": "Unleashed", "in_collection": false, "in_wishlist": false,
+	},
+	{
+		"id": 20, "name": "Swallow", "src": "img/swallow.png", "set": "Secret Wonders", "in_collection": false, "in_wishlist": false,
+	},
+	{
+		"id": 21, "name": "Umbreon", "src": "img/umbreon.png", "set": "Rising Rivals", "in_collection": true, "in_wishlist": false,
+	},
+	{
+		"id": 22, "name": "Wobbuffet", "src": "img/wobbuffet.png", "set": "Unleashed", "in_collection": true, "in_wishlist": false,
+	},
+	{
+		"id": 23, "name": "Wooper", "src": "img/wooper.png", "set": "Neo Destiny", "in_collection": false, "in_wishlist": false,
+	},
+	]
 
-class Card {
-	// TODO: add card set info? too much...
-
-	constructor(name, img) {
-		this._quantity = 0;
-		this._wishlist = false;
-	}
-
-	get quantity ()    { return this._quantity; }
-	get wishlist ()    { return this._wishlist; }
-	set quantity (qnt) { this._quantity = qnt; }
-
-	addToWishlist()      { this._wishlist = true; }
-	removeFromWishList() { this._wishlist = false; }
-}
+};
