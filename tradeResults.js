8d877cac2f7ad9a2d95cc85c90f7a7b1296d9719
Margaret Sands <msands@mit.edu>
2018-05-03 22:36:45 -0400

    added profile page and trade results page

---
+++ tradeResults.js
@@ -0,0 +1,76 @@
+function createPage() {
+	var matches = document.getElementById("matches");
+	var matchData = [{"name": "Ash", "location": "Boston", "want": "Pikachu"}]
+	var numMatches = 1;
+	var numMatchesDiv = document.createElement("div");
+	if (numMatches==1) {
+		numMatchesDiv.innerHTML = "You have " + numMatches + " match!";
+	} else {
+		numMatchesDiv.innerHTML = "You have " + numMatches + " matches!";
+	}
+	matches.append(numMatchesDiv);
+	var info = ["name", "location", "want"]
+	for (var i=0; i<matchData.length; i++) {
+		var match = document.createElement("div");
+		match.className = "match";
+		var image = document.createElement("img");
+		image.className = "profilePicPreview";
+		image.src = "img/" + matchData[i].name + ".png";
+		var infoDiv = document.createElement("div");
+		infoDiv.className = "infoDiv";
+		infoDiv.append(image);
+		for (var j=0; j<info.length; j++) {
+			var singleInfoDiv = document.createElement("div");
+			singleInfoDiv.className = "singleInfoDiv " + info[j];
+			if (info[j]=="location") {
+				singleInfoDiv.innerHTML = "<i class='map pin icon'></i> " + matchData[i][info[j]];
+			} else if (info[j]=="want") {
+				singleInfoDiv.innerHTML = "<i class='star icon'></i> " + matchData[i][info[j]];
+			} else {
+			singleInfoDiv.innerHTML = matchData[i][info[j]];
+		}
+			infoDiv.append(singleInfoDiv);
+		}
+		var outerMessage = document.createElement("div");
+		outerMessage.className = "message";
+		var innerMessage = document.createElement("div");
+		innerMessage.className = "ui button";
+		innerMessage.innerHTML = "Message";
+		outerMessage.append(innerMessage);
+		infoDiv.append(outerMessage);
+		match.append(infoDiv);
+		matches.append(match);
+	}
+	runSemanticJquery();
+}
+
+function runSemanticJquery() {
+       $('.card')
+      .popup({
+        inline: true,
+        position: "bottom center",
+      });
+     $('.text')
+      .popup({
+        inline: true,
+        position: "left center"
+      });
+$('.ui.dropdown')
+  .dropdown({
+    values: [
+      {
+        name: 'Users Who Want',
+        value: 'trade'
+      },
+      {
+        name     : 'Users Who Have',
+        value    : 'wishlist',
+        selected : true
+      }
+    ]
+  })
+;
+}
+
+
+document.addEventListener("DOMContentLoaded",createPage);
