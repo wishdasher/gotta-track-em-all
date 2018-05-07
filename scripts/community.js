7edb12ad5afbc542da6741e507571c9f352e6cf7
Kunyi Li <kunyi@mit.edu>
2018-05-07 00:14:19 -0400

    community

--- scripts/community.js
+++ scripts/community.js
@@ -1,14 +1,30 @@
 var createPage = () => {
-  initMap();
+  createResults("boston");
 }
 document.addEventListener("DOMContentLoaded", createPage);
 
-function initMap() {
-  var mit = {lat: 42.359184, lng: -71.093544};
-  var center = {lat: 42.359184, lng: -71.093544};
-  var map = new google.maps.Map(document.getElementById('map'), { zoom: 13, center: center }); var marker = new google.maps.Marker({ position: mit, map: map });
-  var pandemonium = {lat: 42.366218, lng: -71.105599};
-  var marker2 = new google.maps.Marker({position: pandemonium, map: map});
-  var newEnglandComics = {lat: 42.343377, lng: -71.123253};
-  var marker3 = new google.maps.Marker({position: newEnglandComics, map: map})
+function createResults(location) {
+  var leaguesInLoc;
+  if (location == "boston"){leaguesInLoc = leagues.boston;}
+  if (location == "sanFrancisco"){leaguesInLoc = leagues.sanFrancisco;}
+  for(var i=0; i<leaguesInLoc.length;i++){
+    var league = leaguesInLoc[i];
+    var newLeague = document.createElement("div");
+    newLeague.className = "league";
+
+    var leagueName = document.createElement("div");
+    leagueName.className = "leagueName";
+    leagueName.innerHTML = league.name;
+
+    var leagueAddress = document.createElement("div");
+    leagueAddress.className = "leagueAddress";
+    leagueAddress.innerHTML = league.street;
+
+    newLeague.append(leagueName);
+    newLeague.append(leagueAddress);
+    document.getElementById("mapResults").append(newLeague);
+
+
+  }
+
 }
