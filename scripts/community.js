0a2bbcceb50185a11aae791d2f4ef6446c3b92f6
Kunyi Li <kunyi@mit.edu>
2018-05-07 05:02:23 -0400

    community

--- scripts/community.js
+++ scripts/community.js
@@ -1,30 +1,91 @@
+var leagueLocation;
+var searchDistance;
 var createPage = () => {
-  createResults("boston");
+  createResults("boston",25);
 }
 document.addEventListener("DOMContentLoaded", createPage);
 
-function createResults(location) {
+function runSemanticJquery() {
+  $('.dropdown').dropdown();
+}
+
+function createResults(location, distance) {
   var leaguesInLoc;
   if (location == "boston"){leaguesInLoc = leagues.boston;}
   if (location == "sanFrancisco"){leaguesInLoc = leagues.sanFrancisco;}
+  console.log(location);
+  console.log(leaguesInLoc);
   for(var i=0; i<leaguesInLoc.length;i++){
     var league = leaguesInLoc[i];
-    var newLeague = document.createElement("div");
-    newLeague.className = "league";
+      if (league.distance <= distance){
+      var newLeague = document.createElement("div");
+      newLeague.className = "league";
+      newLeague.className = "ui list";
 
-    var leagueName = document.createElement("div");
-    leagueName.className = "leagueName";
-    leagueName.innerHTML = league.name;
+      var leagueItem = document.createElement("div");
+      leagueItem.className = "leagueItem";
 
-    var leagueAddress = document.createElement("div");
-    leagueAddress.className = "leagueAddress";
-    leagueAddress.innerHTML = league.street;
+      var mapIcon = document.createElement("i");
+      mapIcon.className = "big map marker alternate icon point";
 
-    newLeague.append(leagueName);
-    newLeague.append(leagueAddress);
-    document.getElementById("mapResults").append(newLeague);
+      var content = document.createElement("div");
 
+      var leagueName = document.createElement("a");
+      leagueName.className = "leagueName";
+      leagueName.innerHTML = league.name;
 
+      var leagueAddress = document.createElement("div");
+      leagueAddress.className = "leagueAddress";
+      leagueAddress.innerHTML = league.street + "<br>" + league.cityState + " " + league.zipCode;
+
+      document.getElementById("mapResults").append(newLeague);
+      newLeague.append(leagueItem);
+      leagueItem.append(mapIcon);
+      leagueItem.append(content);
+      content.append(leagueName);
+      content.append(leagueAddress);
+    }
+  }
+}
+
+function searchFunc() {
+  searchTerm = document.getElementById("search").value;
+  searchTerm = searchTerm.toLowerCase();
+  searchTerm = searchTerm.replace(/ /g,"");
+  console.log(searchTerm);
+  if (searchTerm!="" && searchTerm!=null) {
+    var searchDistance = $('#distance-select').dropdown('get value');
+     if (searchTerm == "boston,ma" || searchTerm == "boston,massachusetts"){
+       leagueLocation = "boston";
+     }
+     else if(Number(searchTerm) && 02108 <= Number(searchTerm) &&  Number(searchTerm) <= 02118){
+       leagueLocation = "boston";
+     }
+     else if(searchTerm == "sanfrancisco,ca" || searchTerm == "sanfrancisco,california"){
+       leagueLocation = "sanFrancisco";
+       moveToLocation(37.772703, -122.43739, searchDistance);
+     }
+     else if(Number(searchTerm) && 94102 <= Number(searchTerm) &&  Number(searchTerm) <= 94150){
+       leagueLocation = "sanFrancisco";
+       moveToLocation(37.772703, -122.43739, searchDistance);
+     }
+     document.getElementById("mapResults").innerHTML = "";
+     createResults(leagueLocation,searchDistance);
+  }
+}
+
+function moveToLocation(lat, lng, distance){
+  console.log(distance);
+  map = new google.maps.Map(document.getElementById('map'), { zoom: 12, center: mewtwosday });
+  var mewtwosday = {lat: 37.772703, lng: -122.43739};
+  var marker = new google.maps.Marker({ position: mewtwosday, map: map });
+  if (distance > 5 && distance <= 10){
+    var games = {lat: 37.867881, lng: -122.258463};
+    var marker2 = new google.maps.Marker({ position: games, map: map });
+    var burlinggame = {lat: 37.595973, lng: -122.3851}
+    var marker2 = new google.maps.Marker({ position: burlinggame, map: map });
   }
 
+  // using global variable:
+  map.panTo(mewtwosday);
 }
